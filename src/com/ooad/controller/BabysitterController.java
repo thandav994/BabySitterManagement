e0944f8c9e3d11be46807dedb56ff6a0fd659bc7
Akhila Perabe <axp178830@utdallas.edu>
2017-10-22 06:47:12 +0100

    Updated the DAO and controller Added the sql script

---
+++ src/com/ooad/controller/BabysitterController.java
@@ -0,0 +1,12 @@
+package com.ooad.controller;
+
+import com.ooad.beans.Appointment;
+import com.ooad.beans.User;
+
+public class BabysitterController {
+
+	// BabysitterDAO class
+	//Appointment[] GetListOfAppointments(User user)
+	
+	//UpdateBabySitterApproval(Appointment appointment, Boolean approval)
+}
--- src/com/ooad/dao/BabysitterDAO.java
+++ src/com/ooad/dao/BabysitterDAO.java
@@ -3,28 +3,45 @@ package com.ooad.dao;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
 
 import com.ooad.beans.Appointment;
 import com.ooad.beans.AppointmentStatus;
 import com.ooad.beans.User;
-import com.ooad.entities.BabySitterEntity;
-import com.ooad.entities.LoginEntity;
 import com.ooad.entities.AppointmentEntity;
-import com.ooad.entities.ParentEntity;
 
 public class BabysitterDAO {
 	
 	public Appointment[] GetListOfAppointments(User user) throws ParseException {
+
+		SessionFactory sessionFactory = new Configuration()
+	               .configure("hibernate.cfg.xml").buildSessionFactory();
+		Session session = sessionFactory.openSession();
 		
+		session.beginTransaction();
+		
+		List<AppointmentEntity> appointments = (List<AppointmentEntity>) session.createQuery(
+				"from babysitters natural join appointments where babysitters.emailID=" 
+				+ user.getEmail()).list();
+		
+		session.close();
+		sessionFactory.close();
+		
+		if (appointments != null)
+			return (Appointment[]) appointments.toArray();
+		else
+			return null;
 	}
 	
-	public void UpdateBabySitterApproval(Appointment appointment, Boolean approval) {
+	public void UpdateBabySitterApproval(Appointment appointment, Boolean approval) throws ParseException {
 		
 		SessionFactory sessionFactory = new Configuration()
 	               .configure("hibernate.cfg.xml").buildSessionFactory();
@@ -43,9 +60,10 @@ public class BabysitterDAO {
 			}
 		}
 		
+		session.save(appEntity);
+		session.getTransaction().commit();
 		session.close();
 		sessionFactory.close();
 
 	}
-
 }
